43b125fa53912ea73134c371d739b0ec047883d6
kyawkhantwin <87024094+kyawkhantwin@users.noreply.github.com>
2022-05-24 19:41:31 +0630

    Delete complete-javascript-course-master directory

--- complete-javascript-course-master/18-forkify/final/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '5d1bfb8a-1af7-4a92-8648-a7564a84fd4c';
-export const MODAL_CLOSE_SEC = 2.5;
